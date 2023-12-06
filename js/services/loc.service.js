d47fca1d7377ba19b4ed1e1bee1011d1e9a4c463
Konstantin Nochevny <kosnobiz@gmail.com>
2023-12-06 16:36:54 +0200

    Add loc.service functions

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -4,9 +4,11 @@ export const locService = {
     getLocs
 }
 
+const LOCS_STORAGE_KEY = 'locsDB'
+
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -19,3 +21,22 @@ function getLocs() {
 }
 
 
+function createPlace() {
+    // storageService.post(locsDB, )
+}
+
+function readPlace() {
+
+}
+
+function updatePlace() {
+
+}
+
+function deletePlace() {
+
+}
+
+function listPlace() {
+
+}
